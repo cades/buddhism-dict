37a2adeaf9844399e8adf1c0a2279236815c379f
hong-jen kao <endlessfighting0327@gmail.com>
2014-06-22 15:42:37 +0800

    first commit

--- README.md
+++ README.md
@@ -1,4 +1,28 @@
 yushu-crawler
 =============
 
-中文御書 crawler
+[中文御書](http://cht.sgilibrary.org/index.php) crawler
+
+使用原網站「頁數檢索」的功能, 將 1716 頁的御書一頁一頁轉成 json 格式.
+
+# 使用方式
+
+請先確認有安裝[nodejs](http://nodejs.org/).
+
+`
+npm install
+node crawler.js
+`
+
+等到 script 跑完, 所有資料就會放在 pages/ 目錄下.
+
+# json 格式
+
+```
+{
+  "page": <integer>,
+  "title": <string>,
+  "text": <string>
+}
+```
+
---
+++ crawler.js
@@ -0,0 +1,31 @@
+var fs = require('fs'),
+    util = require('util'),
+    Lazy = require('lazy.js'),
+    request = require('request'),
+    cheerio = require('cheerio'),
+    url_template = 'http://cht.sgilibrary.org/ResultDetail.php?BookArticle=1&pageNo=%s&showNum=50&searchType=3';
+
+function inc (x) { return x + 1; }
+
+function downloadPage(pageNum) {
+  var url = util.format(url_template, pageNum);
+  request({url: url}, function(e, r, html){
+    var $ = cheerio.load(html),
+        title = $('body table tr td table:nth-child(3) tr:nth-child(1) .txt_title').text(),
+        text  = $('body table tr td table:nth-child(3) tr:nth-child(2) .txt').text(),
+        pageJson = {page: pageNum, title: title, text: text};
+
+    // 一頁一頁寫入檔案.
+    fs.writeFile('./pages/' + pageNum + '.json', JSON.stringify(pageJson), function(err){
+      if (err)
+        console.log("Page " + pageNum + " fails to write to file.");
+      else
+        console.log("Page " + pageNum + " downloaded.");
+    });
+
+  });
+}
+
+// 從第 1 頁抓到 1716 頁
+Lazy.range(1716).map(inc).toArray().map(downloadPage);
+
